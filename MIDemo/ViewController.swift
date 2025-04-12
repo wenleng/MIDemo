99d67210e0bdd56bdcc7cc9ac57ec2c63f8b3d67
Weleng <727367641@qq.com>
2025-04-13 05:47:18 +0800

    Initial Commit

---
+++ MIDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MIDemo
+//
+//  Created by 光光 on 2025/4/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MIDemoTests/MIDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MIDemoTests.swift
+//  MIDemoTests
+//
+//  Created by 光光 on 2025/4/13.
+//
+
+import Testing
+@testable import MIDemo
+
+struct MIDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
